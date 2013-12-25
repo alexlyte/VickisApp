489c911c94f167b0ab269e24918f1d8ead1908dc
alexlyte <alexanderlyte@gmail.com>
2013-12-25 10:50:45 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VickisApp::Application.config.secret_key_base = 'b2572050c0b9d6560b99f579896a64ce08be497c8ef30d5c1ab75d29e5c1c2e774696008a7bb9da9cfb153f2fbdb0ab86ad774d324ba0e0048a57a92f4def009'
